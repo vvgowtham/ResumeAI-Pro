4d37a9515d8c7badc2f235762dcc9f03f6a2d988
vvgowtham <vvgowtham292@gmail.com>
2026-07-11 19:48:15 +0530

    fix: resolve all root configuration issues - pnpm workspace, eslint, prettier, commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,12 +1,3 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  rules: {
-    'type-enum': [
-      2,
-      'always',
-      ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore', 'perf', 'ci', 'build', 'revert'],
-    ],
-    'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
-    'header-max-length': [2, 'always', 100],
-  },
 };
